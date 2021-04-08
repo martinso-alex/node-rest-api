cbc23dba803ca8e73abd479f63a003ce80b40949
Alexandre Martins <martinso.alex@gmail.com>
2021-04-08 02:10:35 -0300

    configs e controllers

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const express = require('express')
+const customExpress = require('./configs/customExpress')
 
-const app = express()
+const app = customExpress()
 
 app.listen(3000, () => console.log('server rodando na porta 3000'))
-
-app.get ('/atendimentos', (req, res) => res.send('você está na rota de atendimentos'))
